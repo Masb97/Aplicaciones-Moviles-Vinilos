a3c62b7994aecb63ba0fc6e063923d0c95e75b65
Santiago Aparicio <s.aparicio11@uniandes.edu.co>
2026-05-09 17:07:50 -0500

    unit test de collector

---
+++ app/src/test/java/com/movilesuniandes/vinilos/features/collector/CollectorViewModelTest.kt
@@ -0,0 +1,80 @@
+package com.movilesuniandes.vinilos.features.collector
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.movilesuniandes.vinilos.features.collector.model.CollectorRepository
+import com.movilesuniandes.vinilos.features.collector.viewmodel.CollectorUiState
+import com.movilesuniandes.vinilos.features.collector.viewmodel.CollectorViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.Assert.assertTrue
+import org.junit.Assert.assertEquals
+import com.movilesuniandes.vinilos.features.collector.model.Collector
+
+@OptIn(ExperimentalCoroutinesApi :: class)
+
+
+class CollectorViewModelTest {
+    @get: Rule
+    
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+    private val testDispatcher= UnconfinedTestDispatcher()
+    
+    @Before
+    fun setup(){
+        Dispatchers.setMain(testDispatcher)
+    }
+    
+    @After
+    fun tearDown(){
+        Dispatchers.resetMain()
+    }
+    
+    @Test
+    fun `uiState inicia en loading`(){
+        val repository = object : CollectorRepository {
+            override suspend fun getCollectors(): List<Collector> {
+                kotlinx.coroutines.delay(1000) 
+                return emptyList()
+            }
+        }
+
+        
+        val viewModel = CollectorViewModel(repository)
+        
+        assertTrue(viewModel.uiState.value is CollectorUiState.Loading)
+    }
+
+    @Test
+    fun `loadCollectos con repositorio exitoso emite Success`(){
+        val viewModel= CollectorViewModel(FakeCollectorRepository())
+        assertTrue(viewModel.uiState.value is CollectorUiState.Success)
+    }
+
+    @Test
+    fun `loadCollector con error emite Error`(){
+        val viewModel= CollectorViewModel(FakeCollectorRepositoryWithError())
+        assertTrue(viewModel.uiState.value is CollectorUiState.Error)
+    }
+
+    @Test
+    fun `Sucess contiene los coleccionistas del repositorio`(){
+        val viewModel= CollectorViewModel(FakeCollectorRepository())
+        val state= viewModel.uiState.value as CollectorUiState.Success
+        assertEquals(2, state.collectors.size)
+        assertEquals("Manolo Bellon", state.collectors[0].name)
+    }
+
+    @Test
+    fun `Error contiene el mensaje de la excepcion`(){
+        val viewModel= CollectorViewModel(FakeCollectorRepositoryWithError())
+        val state = viewModel.uiState.value as CollectorUiState.Error
+        assertEquals("Error de conexion", state.message)
+    }
+}
